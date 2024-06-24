e82761994c76efc999402b2b17fd90567608523e
hekhy <titocristian.dev@gmail.com>
2024-06-25 01:27:57 +0200

    Migracion a React

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [checked, setChecked] = useState(true);
+
+  return (
+    <>
+      <div className="pop-up">
+        <h2>Disney+ Skipper</h2>
+
+        <br />
+
+        <label className="checkbox">
+          Activate
+          <input
+            id="skipper-checkbox"
+            type="checkbox"
+            name="checkbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+          />
+          <div className="checkmark"></div>
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default App;
