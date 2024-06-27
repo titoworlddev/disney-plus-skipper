850e2a38edd2ad83617f7e304f8b57b18c9b2f77
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2024-06-27 14:58:28 +0200

    Controlando errores y verificaciones en senMessageToContentScript

--- src/utils/sendMessageToContentScript.ts
+++ src/utils/sendMessageToContentScript.ts
@@ -1,11 +1,27 @@
 export const sendMessageToContentScript = (type: string) => {
+  if (!chrome.tabs) {
+    console.error('Chrome tabs API is not available');
+    return;
+  }
+
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    if (chrome.runtime.lastError) {
+      console.error('Error querying tabs:', chrome.runtime.lastError);
+      return;
+    }
+
     if (tabs.length === 0) {
       console.error('No active tabs found');
       return;
     }
 
-    chrome.tabs.sendMessage(tabs[0].id!, { type }, response => {
+    const tabId = tabs[0].id;
+    if (tabId === undefined) {
+      console.error('Tab ID is undefined');
+      return;
+    }
+
+    chrome.tabs.sendMessage(tabId, { type }, response => {
       if (chrome.runtime.lastError) {
         console.error('Error sending message:', chrome.runtime.lastError);
       } else {
